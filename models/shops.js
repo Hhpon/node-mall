27b31c264074c771971702802d63d19ec4e1a417
李嘉俊 <794857535@qq.com>
2018-02-02 09:18:27 +0800

    更新商品数据模型

--- models/shops.js
+++ models/shops.js
@@ -4,18 +4,13 @@ var schema = new mongoose.Schema({
     productId:String,
     productImage: String,        // 图片地址
     productName: String,      // 商品标题
-    link: String,       // 商品链接
     salePrice: Number,      // 商品价格
+    oldprice:Number,
+    sales:Number,
     productNum:Number,
     checked:String,
     classify:String,
-    date: {             // 时间
-        type: Date,
-        default: Date.now
-    },
-    time: {             // 时间戳
-        type: Number,
-        default: Date.now
-    },
+    details:String,
+    date: String,
 })
 module.exports = mongoose.model('Shops', schema);
